39a4b2254f00fff3701d894780d22e460e3cd01f
EC2 Default User <ec2-user@ip-172-31-74-125.ec2.internal>
2021-11-27 08:32:57 +0000

    [update&finish]メッセージ表示タイミングの修正とページレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    flash[:notice] = "Book was successfully created."
     @book = Book.new(book_params)
     if @book.save
      redirect_to book_path(@book.id)
+     flash[:notice] = "Book was successfully created."
     else
      @books = Book.all
      render :index
@@ -24,9 +24,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    flash[:notice] = "Book was successfully updated."
     @book = Book.find(params[:id])
     if @book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
      redirect_to book_path(@book.id)
     else
      render :edit
